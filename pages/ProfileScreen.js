ee7ea4b652e950b6a0570c73c80ea7c24cb5b43e
kayeongkim1 <61729501+kayeongkim1@users.noreply.github.com>
2021-02-04 23:01:45 +0900

    Add files via upload

--- pages/ProfileScreen.js
+++ pages/ProfileScreen.js
@@ -1,22 +1,40 @@
 import React, { Component, useState } from 'react';
 import { View, StyleSheet  } from 'react-native';
-import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom"; //npm i react-router-dom 로 설치
+import { Link, Route, Switch, BrowserRouter as Router } from 'react-router-dom'; //npm i react-router-dom 로 설치
 import styled from 'styled-components';
 //import { Text, Input, Button, ButtonGroup} from 'react-native-elements';
 
-import Container from 'C:/Users/KKY/Desktop/SmartPT.v2-main/components/Container';
+import Container from '../components/Container';
+import Register from '../components/Register';
+
 //import { ViewArraySharp } from '@material-ui/icons';
 
-function ProfileScreen({ navigation }) {
+const styles = StyleSheet.create({
+   
+});
+
+
+function ProfileScreen({ navigation, Route }) {
   return (
       <Container>
          <View>
-           <Link to="../components/Join">
-             <button>가입하기</button>
-            </Link>
-
+         <TouchableOpacity
+                        onPress={()=> NavigationService.navigate('Register', {
+                            screen: 'Register',
+                            info: 'information'
+                        })}
+                        style={{
+                            justifyContent: 'flex-end',
+                            backgroundColor: 'rgb(87,174,198)',
+                            padding: 20,
+                            marginTop: 20,
+                            borderRadius: 30
+                        }}>
+                        <Text style={{fontSize: 20, textAlign: 'center', color: 'white'}}>회원가입</Text>
+                    </TouchableOpacity>
          </View>
       </Container>
+    
 
   );
   
